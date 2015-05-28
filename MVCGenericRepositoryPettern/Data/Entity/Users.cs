1eef48e0b68aabfe9e191a855ffe1a0c0e673573
Meesam Zaidi <meesam.engineer@gmail.com>
2015-05-28 18:45:05 +0530

    Update Users.cs

--- MVCGenericRepositoryPettern/Data/Entity/Users.cs
+++ MVCGenericRepositoryPettern/Data/Entity/Users.cs
@@ -11,3 +11,26 @@
         public string Password { get; set; }
     }
 }
+<script type="text/javascript">
+        var testData = "";
+        $.ajax({
+            url: "/OrgChart/GetOrgChat",
+            datatype: "json",
+            success: function (jsonresponce) {
+                testData = jsonresponce;
+            }
+        }).done(function(){
+            org_chart = $('#orgChart').orgChart({
+                data: testData,
+                showControls: false,
+                allowEdit: false,
+                onClickNode: function (node) {
+                    $(".node").popover({
+                        title: "Software Engineer",
+                        content: "Since then, software engineering undergraduate degrees have been established at many universities. A standard international curriculum for undergraduate software engineering degrees was recently[when?] defined by the CCSE. As of 2004, in the U.S.",
+                        placement: "right"
+                    });
+                }
+            });
+        });
+    </script>   
